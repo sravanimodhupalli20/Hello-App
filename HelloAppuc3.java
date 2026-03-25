02802aa50bb9037ab6961e995e05d11805e9e945
sravanimodhupalli20 <sravanimodhupalli20@gmail.com>
2026-03-25 08:01:58 +0530

    Project HelloApp

--- HelloAppuc3.java
+++
@@ -1,7 +0,0 @@
-public class HelloAppuc3 {
-    public static void main(String[] args) {
-
-        String name = (args.length > 0) ? args[0] : "World";
-        System.out.println("Hello, " + name + "!");
-    }
-}
---
+++ HelloAppuc4.java
@@ -0,0 +1,9 @@
+public class HelloAppuc4 {
+
+    public static void main(String[] args) {
+
+        // Print message to console
+        System.out.println("Hello World");
+
+    }
+}
